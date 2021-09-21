ab6a467c9cb1ee6a22a953149c63705e122c472d
Álvaro Gomes <alvarofederal@gmail.com>
2021-09-20 22:58:37 -0300

    Commit correção list usuers

--- src/app/usuarios/usuarios-lista/usuarios-lista.component.ts
+++ src/app/usuarios/usuarios-lista/usuarios-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/usuario.service';
+import { Usuario } from '../usuario';
 
 @Component({
   selector: 'app-usuarios-lista',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosListaComponent implements OnInit {
 
-  constructor() { }
+  usuarios: Usuario[] = [];
+
+  constructor(private service: UsuarioService) { }
 
   ngOnInit(): void {
+    /**this.service
+    .getEquipamentos()
+    .subscribe( response => this.equipamentos = response );*/
+    this.service
+    .getUsuarios()
+    .subscribe( response => this.usuarios = response );
+
+    console.log(this.usuarios);
   }
 
 }
